d18efae537416cdb6b8fb2565e6270fbe83991cc
Shivani <ramidishihvani@gmail.com>
2025-05-27 05:03:07 +0530

    Describe your changes

---
+++ creating_array.py
@@ -0,0 +1,13 @@
+n=5
+array = []
+for i in range(n): 
+    array = array + [i]
+print(array)
+
+#calling function
+# Function to print a string n times
+def fun(n,s):
+        for i in range(n):
+            print(s)
+fun(3, "hello")
+
